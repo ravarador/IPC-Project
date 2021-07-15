5671f58b8af03a016327d9d1104c38e59d7996d3
Ravni Arador <ravarador@gmail.com>
2021-07-15 19:51:33 -0400

    added menu and contactManagerSystem function

--- A2/MS2/contactHelpers.c
+++ A2/MS2/contactHelpers.c
@@ -113,11 +113,76 @@ int yes(void) {
 
 // menu: Empty function definition goes here:
 int menu(void) {
-
+    puts("Contact Management System");
+    puts("-------------------------");
+    puts("1. Display contacts");
+    puts("2. Add a contact");
+    puts("3. Update a contact");
+    puts("4. Delete a contact");
+    puts("5. Search contacts by cell phone number");
+    puts("6. Sort contacts by cell phone number");
+    puts("0. Exit");
+    puts("");
+    puts("Select an option:> ");
+    return getIntInRange(0, 6);
 };
 
 
 // contactManagerSystem: Empty function definition goes here:
 void contactManagerSystem(void) {
 
+    int flag = 0; 
+
+    do
+    {
+        switch (menu())
+        {
+        case 0:
+            printf("\nExit the program? (Y)es/(N)o: ");
+            flag = yes();
+            puts("");
+            break;
+
+        case 1:
+            puts("\n<<< Feature 1 is unavailable >>>\n");
+            pause();
+            puts("");
+            break;
+
+        case 2:
+            puts("\n<<< Feature 2 is unavailable >>>\n");
+            pause();
+            puts("");
+            break;
+
+        case 3:
+            puts("\n<<< Feature 3 is unavailable >>>\n");
+            pause();
+            puts("");
+            break;
+
+        case 4:
+            puts("\n<<< Feature 4 is unavailable >>>\n");
+            pause();
+            puts("");
+            break;
+
+        case 5:
+            puts("\n<<< Feature 5 is unavailable >>>\n");
+            pause();
+            puts("");
+            break;
+
+        case 6:
+            puts("\n<<< Feature 6 is unavailable >>>\n");
+            pause();
+            puts("");
+            break;
+
+        default:
+            break;
+        }
+    } while (!flag);
+
+    puts("Contact Management System: terminated");
 };
