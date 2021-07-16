add9a3a4889198f556d386285f2308d0adb80246
Ravni Arador <ravarador@gmail.com>
2021-07-15 20:05:40 -0400

    added code to getContact

--- A2/MS2/contacts.c
+++ A2/MS2/contacts.c
@@ -117,5 +117,7 @@ void getNumbers(struct Numbers* numbers)
 // getContact:
 // Define Empty function definition below:
 void getContact(struct Contact* contact) {
-
+    getName(&contact->name);
+    getAddress(&contact->address);
+    getNumbers(&contact->numbers);
 };
