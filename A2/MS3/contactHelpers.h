4e371782f9ee44320336862c8077c8b3181c193b
Ravni Arador <ravarador@gmail.com>
2021-07-21 15:37:21 -0400

    ms3: added function prototypes in contactHelpers

--- A2/MS3/contactHelpers.h
+++ A2/MS3/contactHelpers.h
@@ -60,34 +60,36 @@ int findContactIndex(const struct Contact contacts[], int size, const char cellN
 
 // displayContactHeader
 // put function prototype here:
-
+void displayContactHeader(void);
 
 // displayContactFooter:
 // put function prototype here:
-
+void displayContactFooter(int count);
 
 // displayContact:
 // put function prototype here:
-
+void displayContact(const struct Contact* contact);
 
 // displayContacts:
 // put function prototype here:
-
+void displayContacts(const struct Contact contacts[], int size);
 
 // searchContacts:
 // put function prototype here:
-
+void searchContacts(const struct Contact contacts[], int size);
 
 // addContact:
 // put function prototype here:
-
+void addContact(struct Contact contacts[], int size);
 
 // updateContact:
 // put function prototype here:
-
+void updateContact(struct Contact contacts[], int size);
 
 // deleteContact:
 // put function prototype here:
+void deleteContact(struct Contact contacts[], int size);
 
 // sortContacts:
 // put function prototype here:
+void sortContacts(struct Contact contacts[], int size);
